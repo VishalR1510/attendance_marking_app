aa76294f59f036a1f693da416df6bb585d6b1170
VishalR1510 <vishalr151005@gmail.com>
2026-03-09 01:45:55 +0530

    Updated backend to connect frontend url

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,11 @@ app = FastAPI(
 # ============================================
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "https://attendance-marking-frontend.onrender.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
